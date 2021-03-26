0da93671731c89817b8c5cda0a779ea9e0f3574c
Vidhi Chauhan <vidhi23chauhan@gmail.com>
2021-03-26 17:16:00 +0530

    First Commit

---
+++ 03_datatypes.py
@@ -0,0 +1,8 @@
+name = "Vidhi" # String
+score = 90.4   # float
+position = 2   # integer
+
+print("Hello " + name + " you got " + str(score) + " score and your position is " + str(position))
+
+
+print("Hello " , name , " you got " , str(score) , " score and your position is " , str(position))
---
+++ 04_concatinations.py
@@ -0,0 +1,9 @@
+name = "Vidhi" # String
+score = 90.4   # float
+position = 2   # integer
+
+print("Hello " + name + " you got " + str(score) + " score and your position is " + str(position))
+
+print("Hello " , name , " you got " , str(score) , " score and your position is " , str(position))
+
+print(f"Hello {name} you got {score} score and your position is {position}")
